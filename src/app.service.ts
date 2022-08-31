ecf7c9f1089f3c8ebeee11685688bd70c1d789ac
r-scheele <abdulrahmanolamilekan88@gmail.com>
2022-08-31 21:36:05 +0100

    project start

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
+// @ts-ignore
+
 import { Injectable } from '@nestjs/common';
+import { CreateReviewDto } from './create-review.dto';
+import aposToLexForm from 'apos-to-lex-form';
+import { WordTokenizer } from 'natural';
+import SpellCorrector from 'spelling-corrector';
+import { removeStopwords } from 'stopword';
+import natural from 'natural';
+
+const spellCorrector = new SpellCorrector();
+spellCorrector.loadDictionary();
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  analyzeReview() {
+
+
   }
 }
